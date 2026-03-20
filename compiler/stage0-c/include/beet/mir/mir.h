ecc07644c57cacf1dd85f7635a1d69bfc39ea10c
jcarraror <julio.a.carraro@gmail.com>
2026-03-20 16:19:21 -0300

    feat(mir): add minimal function MIR skeleton

---
+++ compiler/stage0-c/include/beet/mir/mir.h
@@ -0,0 +1,64 @@
+#ifndef BEET_MIR_MIR_H
+#define BEET_MIR_MIR_H
+
+#include <stddef.h>
+
+#include "beet/parser/ast.h"
+
+#define BEET_MIR_MAX_INSTRS 256
+#define BEET_MIR_MAX_LOCALS 128
+#define BEET_MIR_MAX_NAME_LEN 64
+
+typedef enum beet_mir_opcode {
+    BEET_MIR_OP_CONST_INT = 0,
+    BEET_MIR_OP_BIND_LOCAL,
+    BEET_MIR_OP_RETURN_LOCAL,
+    BEET_MIR_OP_RETURN_CONST_INT
+} beet_mir_opcode;
+
+typedef struct beet_mir_instr {
+    beet_mir_opcode op;
+    int dst;
+    int int_value;
+    char name[BEET_MIR_MAX_NAME_LEN];
+} beet_mir_instr;
+
+typedef struct beet_mir_function {
+    char name[BEET_MIR_MAX_NAME_LEN];
+    beet_mir_instr instrs[BEET_MIR_MAX_INSTRS];
+    size_t instr_count;
+
+    char locals[BEET_MIR_MAX_LOCALS][BEET_MIR_MAX_NAME_LEN];
+    size_t local_count;
+
+    int next_temp;
+} beet_mir_function;
+
+void beet_mir_function_init(beet_mir_function *function, const char *name, size_t name_len);
+
+int beet_mir_add_const_int(beet_mir_function *function, int value);
+int beet_mir_add_bind_local(
+    beet_mir_function *function,
+    const char *name,
+    size_t name_len,
+    int src_temp
+);
+int beet_mir_add_return_local(
+    beet_mir_function *function,
+    const char *name,
+    size_t name_len
+);
+int beet_mir_add_return_const_int(beet_mir_function *function, int value);
+
+int beet_mir_lower_binding(
+    beet_mir_function *function,
+    const beet_ast_binding *binding
+);
+
+int beet_mir_lower_trivial_return_function(
+    beet_mir_function *function,
+    const beet_ast_function *function_ast,
+    int return_value
+);
+
+#endif
---
+++ tests/mir_lowering.c
@@ -0,0 +1,95 @@
+#include <assert.h>
+#include <string.h>
+
+#include "beet/mir/mir.h"
+#include "beet/parser/parser.h"
+#include "beet/support/source.h"
+
+static void test_lower_binding_to_mir(void) {
+    const char *text = "bind x = 10\n";
+    beet_source_file file;
+    beet_parser parser;
+    beet_ast_binding binding;
+    beet_mir_function function;
+
+    beet_source_file_init(&file);
+    assert(beet_source_file_set_text_copy(&file, "test.beet", text));
+    beet_parser_init(&parser, &file);
+
+    assert(beet_parser_parse_binding(&parser, &binding));
+
+    beet_mir_function_init(&function, "main", 4U);
+    assert(beet_mir_lower_binding(&function, &binding));
+
+    assert(function.instr_count == 2U);
+
+    assert(function.instrs[0].op == BEET_MIR_OP_CONST_INT);
+    assert(function.instrs[0].dst == 0);
+    assert(function.instrs[0].int_value == 10);
+
+    assert(function.instrs[1].op == BEET_MIR_OP_BIND_LOCAL);
+    assert(function.instrs[1].dst == 0);
+    assert(strcmp(function.instrs[1].name, "x") == 0);
+
+    assert(function.local_count == 1U);
+    assert(strcmp(function.locals[0], "x") == 0);
+
+    beet_source_file_dispose(&file);
+}
+
+static void test_lower_mutable_binding_to_mir(void) {
+    const char *text = "mutable total = 25\n";
+    beet_source_file file;
+    beet_parser parser;
+    beet_ast_binding binding;
+    beet_mir_function function;
+
+    beet_source_file_init(&file);
+    assert(beet_source_file_set_text_copy(&file, "test.beet", text));
+    beet_parser_init(&parser, &file);
+
+    assert(beet_parser_parse_binding(&parser, &binding));
+
+    beet_mir_function_init(&function, "main", 4U);
+    assert(beet_mir_lower_binding(&function, &binding));
+
+    assert(function.instr_count == 2U);
+    assert(function.instrs[0].op == BEET_MIR_OP_CONST_INT);
+    assert(function.instrs[0].int_value == 25);
+    assert(function.instrs[1].op == BEET_MIR_OP_BIND_LOCAL);
+    assert(strcmp(function.instrs[1].name, "total") == 0);
+
+    beet_source_file_dispose(&file);
+}
+
+static void test_lower_trivial_function_return(void) {
+    const char *text =
+        "function main() returns Int {\n"
+        "    return 0\n"
+        "}\n";
+    beet_source_file file;
+    beet_parser parser;
+    beet_ast_function function_ast;
+    beet_mir_function mir_function;
+
+    beet_source_file_init(&file);
+    assert(beet_source_file_set_text_copy(&file, "test.beet", text));
+    beet_parser_init(&parser, &file);
+
+    assert(beet_parser_parse_function(&parser, &function_ast));
+    assert(beet_mir_lower_trivial_return_function(&mir_function, &function_ast, 0));
+
+    assert(strcmp(mir_function.name, "main") == 0);
+    assert(mir_function.instr_count == 1U);
+    assert(mir_function.instrs[0].op == BEET_MIR_OP_RETURN_CONST_INT);
+    assert(mir_function.instrs[0].int_value == 0);
+
+    beet_source_file_dispose(&file);
+}
+
+int main(void) {
+    test_lower_binding_to_mir();
+    test_lower_mutable_binding_to_mir();
+    test_lower_trivial_function_return();
+    return 0;
+}
