3e765afac9ee10bd1e3de4a4ae16905db172e422
jcarraror <julio.a.carraro@gmail.com>
2026-03-21 07:57:39 -0300

    feat(vm): execute branch bytecode

--- runtime/vm/src/interpreter.c
+++ runtime/vm/src/interpreter.c
@@ -3,6 +3,59 @@
 #include <assert.h>
 #include <string.h>
 
+static size_t beet_vm_instr_width(int op) {
+  switch (op) {
+  case BEET_BC_OP_CONST_INT:
+  case BEET_BC_OP_STORE_LOCAL:
+  case BEET_BC_OP_LOAD_LOCAL:
+  case BEET_BC_OP_JUMP_IF_FALSE:
+    return 3U;
+
+  case BEET_BC_OP_ADD_INT:
+  case BEET_BC_OP_SUB_INT:
+  case BEET_BC_OP_MUL_INT:
+  case BEET_BC_OP_DIV_INT:
+    return 4U;
+
+  case BEET_BC_OP_RETURN_LOCAL:
+  case BEET_BC_OP_RETURN_TEMP:
+  case BEET_BC_OP_RETURN_CONST_INT:
+  case BEET_BC_OP_LABEL:
+  case BEET_BC_OP_JUMP:
+    return 2U;
+
+  default:
+    return 0U;
+  }
+}
+
+static int beet_vm_find_label_pc(const beet_bytecode_function *function,
+                                 int label_id, size_t *out_pc) {
+  size_t pc;
+
+  assert(function != NULL);
+  assert(out_pc != NULL);
+
+  pc = 0U;
+  while (pc < function->code_count) {
+    int op = function->code[pc];
+    size_t width = beet_vm_instr_width(op);
+
+    if (width == 0U || pc + width > function->code_count) {
+      return 0;
+    }
+
+    if (op == BEET_BC_OP_LABEL && function->code[pc + 1U] == label_id) {
+      *out_pc = pc + width;
+      return 1;
+    }
+
+    pc += width;
+  }
+
+  return 0;
+}
+
 int beet_vm_execute(beet_vm *vm, const beet_bytecode_function *function,
                     int *out_result) {
   size_t pc;
@@ -73,6 +126,29 @@ int beet_vm_execute(beet_vm *vm, const beet_bytecode_function *function,
         return 0;
       }
       vm->regs[dst] = vm->regs[lhs] / vm->regs[rhs];
+      break;
+    }
+
+    case BEET_BC_OP_LABEL:
+      pc += 1U;
+      break;
+
+    case BEET_BC_OP_JUMP: {
+      int label_id = function->code[pc++];
+      if (!beet_vm_find_label_pc(function, label_id, &pc)) {
+        return 0;
+      }
+      break;
+    }
+
+    case BEET_BC_OP_JUMP_IF_FALSE: {
+      int condition_temp = function->code[pc++];
+      int label_id = function->code[pc++];
+      if (vm->regs[condition_temp] == 0) {
+        if (!beet_vm_find_label_pc(function, label_id, &pc)) {
+          return 0;
+        }
+      }
       break;
     }
 
--- tests/vm_execution.c
+++ tests/vm_execution.c
@@ -95,6 +95,67 @@ static void test_divide_by_zero_fails(void) {
   assert(!beet_vm_execute(&vm, &fn, &result));
 }
 
+static void test_jump_skips_instructions(void) {
+  beet_bytecode_function fn;
+  beet_vm vm;
+  int result;
+
+  beet_bytecode_function_init(&fn);
+
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_JUMP, 0));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_RETURN_CONST_INT, 99));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_LABEL, 0));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_RETURN_CONST_INT, 7));
+
+  assert(beet_vm_execute(&vm, &fn, &result));
+  assert(result == 7);
+}
+
+static void test_jump_if_false_takes_branch(void) {
+  beet_bytecode_function fn;
+  beet_vm vm;
+  int result;
+
+  beet_bytecode_function_init(&fn);
+
+  assert(beet_bytecode_emit3(&fn, BEET_BC_OP_CONST_INT, 0, 0));
+  assert(beet_bytecode_emit3(&fn, BEET_BC_OP_JUMP_IF_FALSE, 0, 0));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_RETURN_CONST_INT, 1));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_LABEL, 0));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_RETURN_CONST_INT, 2));
+
+  assert(beet_vm_execute(&vm, &fn, &result));
+  assert(result == 2);
+}
+
+static void test_jump_if_false_falls_through_on_true(void) {
+  beet_bytecode_function fn;
+  beet_vm vm;
+  int result;
+
+  beet_bytecode_function_init(&fn);
+
+  assert(beet_bytecode_emit3(&fn, BEET_BC_OP_CONST_INT, 0, 1));
+  assert(beet_bytecode_emit3(&fn, BEET_BC_OP_JUMP_IF_FALSE, 0, 0));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_RETURN_CONST_INT, 1));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_LABEL, 0));
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_RETURN_CONST_INT, 2));
+
+  assert(beet_vm_execute(&vm, &fn, &result));
+  assert(result == 1);
+}
+
+static void test_jump_to_missing_label_fails(void) {
+  beet_bytecode_function fn;
+  beet_vm vm;
+  int result;
+
+  beet_bytecode_function_init(&fn);
+
+  assert(beet_bytecode_emit2(&fn, BEET_BC_OP_JUMP, 9));
+  assert(!beet_vm_execute(&vm, &fn, &result));
+}
+
 int main(void) {
   test_return_const();
   test_store_and_return_local();
@@ -102,5 +163,9 @@ int main(void) {
   test_arithmetic_return_temp();
   test_load_local_and_return_temp();
   test_divide_by_zero_fails();
+  test_jump_skips_instructions();
+  test_jump_if_false_takes_branch();
+  test_jump_if_false_falls_through_on_true();
+  test_jump_to_missing_label_fails();
   return 0;
 }
